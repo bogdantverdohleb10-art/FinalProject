eccca5ccd44e89cc0af45ff6034fab232cbb4aa1
Богдан Твердохліб <bogdantverdohleb10@gmail.com>
2026-08-24 01:08:31 +0300

    Поправки

--- rent/forms.py
+++ rent/forms.py
@@ -9,17 +9,6 @@ class UserRegisterForm(UserCreationForm):
         fields = ['username']
 
 
-from django import forms
-from django.contrib.auth.models import User
-from django.contrib.auth.forms import UserCreationForm
-
-
-class UserRegisterForm(UserCreationForm):
-    class Meta:
-        model = User
-        fields = ['username']
-
-
 class SearchForm(forms.Form):
     STATE_CHOICES = [
         ('', 'Всі штати'),
